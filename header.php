92bacef0be3df9e597bb4b1f6f06b8e31efa9508
Sefa Y. Baach-Acheamphour <sacheamphour@Aluminium.NEWMARWINDOWS.local>
2022-04-05 12:47:44 -0400

    Misc Updates

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
         <div class="site-header__menu group">
       <!-- if you want to make the navigate menu to be  controled from the
              word press admin, dynamic word press menu.
-        -->
+        -->  
           <nav class="main-navigation">
             <!-- <?php wp_nav_menu(array(
             'theme_location'=>  'headerMenuLocation'
@@ -24,9 +24,14 @@
             <ul>
             <li <?php if(is_page('about-us') or wp_get_post_parent_id(0)==13) echo 'class="current-menu-item"'?>>
             <a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+            
               <li><a href="#">Programs</a></li>
-              <li><a href="#">Events</a></li>
+
+              <li <?php if(get_post_type()=='event') echo 'class="current-menu-item"'?>>
+              <a href="<?php echo get_post_type_archive_link('event')?>">Events</a></li>
+
               <li><a href="#">Campuses</a></li> 
+
               <li <?php if(get_post_type()=='post') echo 'class="current-menu-item"'?>>
               <a href="<?php echo site_url('/blog')?>">Blog</a></li>
             </ul>
