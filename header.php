07d130008296e874531338aa6b51906dd5fe2051
Sefa Y. Baach-Acheamphour <sacheamphour@Aluminium.NEWMARWINDOWS.local>
2022-04-05 10:36:14 -0400

    just finished cutom queries

--- header.php
+++ header.php
@@ -22,11 +22,13 @@
             'theme_location'=>  'headerMenuLocation'
             ));?> -->
             <ul>
-            <li <?php if(is_page('about-us') or wp_get_post_parent_id(0)==13) echo 'class="current-menu-item"'?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+            <li <?php if(is_page('about-us') or wp_get_post_parent_id(0)==13) echo 'class="current-menu-item"'?>>
+            <a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
-              <li><a href="#">Campuses</a></li>
-              <li><a href="#">Blog</a></li>
+              <li><a href="#">Campuses</a></li> 
+              <li <?php if(get_post_type()=='post') echo 'class="current-menu-item"'?>>
+              <a href="<?php echo site_url('/blog')?>">Blog</a></li>
             </ul>
           </nav>
           <div class="site-header__util">
