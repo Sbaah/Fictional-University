c4ecb081230cd8f0634f26807b065e8bffcc3db8
Sefa Y. Baach-Acheamphour <sacheamphour@Aluminium.NEWMARWINDOWS.local>
2022-04-12 16:53:01 -0400

    cleaned code and removed  duplication

--- functions.php
+++ functions.php
@@ -1,4 +1,36 @@
 <?php
+function pageBanner($args = NULL) {
+  //ckeck the array of args to if it has a custome value
+  if (!$args['title']) {
+    $args['title'] = get_the_title();
+  }
+
+  if (!$args['subtitle']) {
+    $args['subtitle'] = get_field('page_banner_subtitle');
+  }
+
+  if (!$args['photo']) {
+    if (get_field('page_banner_background_image') AND !is_archive() AND !is_home() ) {
+      $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+    } else {
+      $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+    }
+  }
+
+  ?>
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+      <div class="page-banner__intro">
+        <p><?php echo $args['subtitle']; ?></p>
+      </div>
+    </div>  
+  </div>
+
+  <?php
+}
+
 
 function university_files() {
 
@@ -18,6 +50,9 @@ function university_features(){
   // register_nav_menu('footerLocationTwo','Footer Location Two');
   add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
+  add_image_size('professorLandscape', 400, 260, true);
+  add_image_size('professorPortrait', 480, 650, true);
+  add_image_size('pageBanner',1500,350,true);
 }
 add_action('after_setup_theme', 'university_features');
 
