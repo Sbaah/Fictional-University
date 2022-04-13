3093dedc61b52ca5c21dce87cf41dcc51d0d39c1
Sefa Y. Baach-Acheamphour <sacheamphour@Aluminium.NEWMARWINDOWS.local>
2022-04-13 16:17:07 -0400

    Started on the live search functionality of the site

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '0e6b28bc4d06a27e1e76c9dc3cf609e1');
+<?php return array('dependencies' => array(), 'version' => '36a891de129070fc0ecbfc737c93fea1');
