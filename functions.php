bb9cf9dfb32261bfb60e0628a1d38c864d87088b
Sbaah <sbaah12@gmail.com>
2022-04-27 12:41:23 -0400

    completed the User Roles and Premissions

--- functions.php
+++ functions.php
@@ -122,3 +122,49 @@ add_action( 'pre_get_posts', 'university_adjust_queries');
 add_filter('acf/fields/google_map/api', 'universityMapKey');
 */ 
 
+
+// Redirect subscriber accounts out of admin and onto homepage
+add_action('admin_init', 'redirectSubsToFrontend');
+
+function redirectSubsToFrontend() {
+  $ourCurrentUser = wp_get_current_user();
+
+  // if the current user has a single role
+  //  and the role is subcriber
+  if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+    wp_redirect(site_url('/'));
+    exit;
+  }
+}
+
+//  to now the admin bar
+add_action('wp_loaded', 'noSubsAdminBar');
+
+function noSubsAdminBar() {
+  $ourCurrentUser = wp_get_current_user();
+
+   // if the current user has a single role
+  //  and the role is subcriber
+  if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+    show_admin_bar(false);
+  }
+}
+
+//  Customize Login / SignUp Screen
+add_filter('login_headerurl', 'ourHeaderUrl');
+
+function ourHeaderUrl (){
+  return esc_url(site_url('/'));
+}
+
+add_action('login_enqueue_scripts','ourLoginSignUpCSS');
+function ourLoginSignUpCSS(){
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+  wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+}
+add_filter('login_headertitle','ourLoginSignUpTitle');
+function ourLoginSignUpTitle (){
+  return get_bloginfo('name');
+}
